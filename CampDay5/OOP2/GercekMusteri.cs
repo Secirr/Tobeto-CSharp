2030d28a8112c88f55d2479dc0cb838591c2d5b0
Kadir Özdemir <kadirozdemirr00@gmail.com>
2023-11-10 02:31:51 +0300

    Adding day 5

---
+++ CampDay5/OOP2/GercekMusteri.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace OOP2
+{
+	public class GercekMusteri:Musteri
+	{
+		public string Adi { get; set; }
+		public string SoyAdi { get; set; }
+		public string TcNo { get; set; }
+
+    }
+}
+
